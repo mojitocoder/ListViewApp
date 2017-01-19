69b5fb98f173e534cb480c69e13796a27e5c6438
Quynh Nguyen <qn@rosslynanalytics.com>
2017-01-19 09:49:43 +0000

    Simple list working

--- ListViewApp/ListViewAppPage.xaml.cs
+++ ListViewApp/ListViewAppPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Xamarin.Forms;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace ListViewApp
 {
@@ -13,8 +14,31 @@ namespace ListViewApp
 			var listViewComponent = new ListView();
 			listViewComponent.ItemsSource = new PluralSightTraining().GetAllCourses();//.Select(foo => foo.Name);
 
+
+			// *************************************
+			// ListView supports the following events
+			// *************************************
+			//ItemAppearing: Occurs when the visual representation of an item is being added to the visual layout.
+			//ItemDisappearing: Occurs when the visual representation of an item is being removed from the visual layout.
+			//ItemSelected: Event that is raised when a new item is selected.
+			//ItemTapped:  Event that is raised when an item is tapped.
+			//Refreshing:  Event that is raised when the list view refreshes.
+
+			//attach an event-handler
+			listViewComponent.ItemSelected += ItemSelectedEventHandler;
+
+
 			this.Content = listViewComponent;
 			//InitializeComponent();
+		}
+
+		public void ItemSelectedEventHandler(object sender, SelectedItemChangedEventArgs e)
+		{
+			if (e.SelectedItem != null)
+			{
+				Debug.WriteLine($"ItemSelected: {e.SelectedItem}");
+				(sender as ListView).SelectedItem = null;
+			}
 		}
 	}
 }
